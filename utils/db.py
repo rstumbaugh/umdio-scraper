fb5c6fbdc6ca9b1d449dadd625cacef1ff82afc1
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-11-06 12:02:14 -0500

    create module to insert values into DB

---
+++ utils/db.py
@@ -0,0 +1,14 @@
+
+
+def insert_courses(db, courses):
+	collection = db.courses
+	collection.delete_many({})
+	courses_as_dicts = [course.as_dict() for course in courses.values()]
+	result = collection.insert_many(courses_as_dicts)
+	print('{} course records inserted'.format(len(result.inserted_ids)))
+
+def insert_professors(db, professors):
+	print('inserting profs...')
+
+def insert_departments(db, departments):
+	print('inserting depts...')
