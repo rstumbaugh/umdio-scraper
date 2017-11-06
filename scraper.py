38dda1e931daf1982f86a23daaed266e06fd5c93
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-11-06 13:06:27 -0500

    add gened parsing and rating logic to main

--- scraper.py
+++ scraper.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 from utils.course import get_courses
 from utils.professor import get_professors
 from utils.gen_ed import get_gen_eds
+from utils.db import insert_courses, insert_professors, insert_departments
+from utils.rating import prep_credentials, link_ratings, cleanup_credentials
 
 client = MongoClient(env.get('DB_URI'))
 db = client[env.get('DB_NAME')]
@@ -19,9 +21,9 @@ if current_year >= 9:
 	current_year += 1
 
 years = range(current_year - 3, current_year + 1)
-# semesters = [str(year) + sem for year in years for sem in semester_codes]
-semesters = ['201608']
+semesters = [str(year) + sem for year in years for sem in semester_codes]
 
+# get courses and professors for all semesters
 courses = get_courses(semesters)
 (courses, professors) = get_professors(courses, semesters)
 
@@ -30,4 +32,13 @@ print('found {} professors'.format(len(professors)))
 print('grabbing gen eds...')
 sys.stdout.flush()
 
+# link gen eds for all courses (umd.io gen ed parsing not great)
 courses = get_gen_eds(courses)
+
+# link ratings to all courses
+prep_credentials()
+link_ratings(courses, '')
+cleanup_credentials()
+
+# insert courses into DB
+insert_courses(db, courses)
